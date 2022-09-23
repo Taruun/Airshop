f965aa387053935023732e25a3c117ba2f0baa35
Taruun <phantmetic@gmail.com>
2022-09-23 10:10:34 +0530

    Updated styles

--- components/Nav.js
+++ components/Nav.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { GiShoppingBag } from "react-icons/gi";
 import styled from "styled-components";
 import Cart from "./Cart";
+import User from "./User";
 import { useStateContext } from "../lib/context";
 const { AnimatePresence, motion } = require("framer-motion");
 
@@ -13,24 +14,28 @@ export default function Nav() {
         <h1>
           <Link href={"/"}>Airshop</Link>
         </h1>
-
-        <ShoppingBag onClick={() => setShowCart(true)}>
-          {totalQuantities > 0 && (
-            <motion.span initial={{ scale: 0 }} animate={{ scale: 1 }}>
-              {totalQuantities}
-            </motion.span>
-          )}
-          <GiShoppingBag />
-        </ShoppingBag>
-
+        <Menu>
+          <User />
+          <ShoppingBag onClick={() => setShowCart(true)}>
+            {totalQuantities > 0 && (
+              <motion.span initial={{ scale: 0 }} animate={{ scale: 1 }}>
+                {totalQuantities}
+              </motion.span>
+            )}
+            <GiShoppingBag />
+          </ShoppingBag>
+        </Menu>
         <AnimatePresence> {showCart && <Cart />}</AnimatePresence>
       </InnerNav>
     </NavWrapper>
   );
 }
 const NavWrapper = styled.nav`
-  width: 90vw;
+  width: 100vw;
+  background: white;
+  position: sticky;
   top: 0;
+  margin: 0 calc(-50vw + 50%);
   padding: 1rem 2rem;
 `;
 
@@ -42,14 +47,17 @@ const InnerNav = styled.div`
   justify-content: space-between;
 `;
 
-const ShoppingBag = styled.div`
-  position: relative;
-  cursor: pointer;
+const Menu = styled.div`
+  display: flex;
   svg {
     width: 2rem;
     font-size: 1.5rem;
-    cursor: pointer;
   }
+`;
+
+const ShoppingBag = styled.div`
+  position: relative;
+  cursor: pointer;
   span {
     background: #ff2626;
     color: white;
---
+++ components/User.js
@@ -0,0 +1,43 @@
+import { FaUserCircle } from "react-icons/fa";
+
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import { useUser } from "@auth0/nextjs-auth0/dist/frontend/use-user";
+
+export default function User() {
+  const route = useRouter();
+  const { user } = useUser();
+  if (!user)
+    return (
+      <StyledUser
+        onClick={() => {
+          route.push("/api/auth/login");
+        }}
+      >
+        <FaUserCircle />
+      </StyledUser>
+    );
+
+  return (
+    <Profile title={user.name} onClick={() => route.push("/profil")}>
+      <img src={user.picture} alt={user.name} />
+    </Profile>
+  );
+}
+
+const StyledUser = styled.div`
+  cursor: pointer;
+  padding: 0 0.5rem;
+  margin: 0 0.5rem;
+`;
+
+const Profile = styled.div`
+  padding: 0 0.5rem;
+  margin: 0 0.5rem;
+  cursor: pointer;
+  img {
+    border-radius: 50%;
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+`;
