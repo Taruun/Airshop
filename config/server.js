c17e4dd05befc4508e00b19f3c7571203144cb23
Taruun <phantmetic@gmail.com>
2023-01-02 14:26:20 +0530

    Updated Yarn lockfile

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      ("Sirx2LmBPe/e/dKVJOgKaA==",
+      "JrykZfKctUCpmujPZKOA+A==",
+      "KQbRf8Hp39xlr74xXQjhBg==",
+      "f4mkHNelJl9dxNWhKdWRSQ=="),
+    ]),
   },
 });
